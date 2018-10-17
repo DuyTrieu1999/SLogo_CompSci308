a4b1f18351e724615f468a4b2b432150569ec974
Brooke Keene <bzk2@duke.edu>
2018-10-17 00:17:50 -0500

    refactoring magic values to resource file

--- src/view/DropDownButtons.java
+++ src/view/DropDownButtons.java
@@ -6,20 +6,30 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 
+import java.util.ResourceBundle;
+
 public class DropDownButtons extends VBox {
+    public static final String RESOURCE_PACKAGE = "resources/text/view";
+    private ResourceBundle myResources;
     private ChoiceBox<String> langCB;
 
     public DropDownButtons() {
+        myResources = ResourceBundle.getBundle(RESOURCE_PACKAGE);
         this.setId("dropdown-menu");
         this.getChildren().add(addControls());
         this.getChildren().add(addLanguage());
     }
     private ChoiceBox addLanguage () {
         langCB = new ChoiceBox<>();
-        langCB.setPrefWidth(SceneENUM.DROPDOWN_WIDTH.getVal());
-        langCB.getItems().add("English");
-        langCB.getItems().add("French");
-        langCB.getItems().add("Spanish");
+        langCB.setPrefWidth(Integer.parseInt(myResources.getString("Dropdown_Width")));
+        langCB.getItems().add(myResources.getString("Chinese")); //TODO: find better way to list all choices rather than just hardcoding
+        langCB.getItems().add(myResources.getString("English"));
+        langCB.getItems().add(myResources.getString("French"));
+        langCB.getItems().add(myResources.getString("German"));
+        langCB.getItems().add(myResources.getString("Italian"));
+        langCB.getItems().add(myResources.getString("Portuguese"));
+        langCB.getItems().add(myResources.getString("Russian"));
+        langCB.getItems().add(myResources.getString("Spanish"));
         langCB.setValue("English");
         langCB.setOnAction(e -> getChoice(langCB));
         return langCB;
@@ -32,7 +42,7 @@ public class DropDownButtons extends VBox {
         VBox controlBox = new VBox();
         controlBox.getChildren().add(new Button("bla"));
         controlBox.getChildren().add(new Button("bla"));
-        tp.setText("Controls");
+        tp.setText(myResources.getString("Controls"));
         tp.setContent(controlBox);
         tp.setExpanded(false);
         return tp;
--- src/view/LogoButton.java
+++ src/view/LogoButton.java
@@ -4,12 +4,18 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 
+import java.util.ResourceBundle;
+
 public class LogoButton extends Button {
+    public static final String RESOURCE_PACKAGE = "resources/text/view";
+    private ResourceBundle myResources;
 
     public LogoButton (String buttonName, EventHandler<ActionEvent> event) {
+        myResources = ResourceBundle.getBundle(RESOURCE_PACKAGE);
+
         this.setText(buttonName);
-        this.setMinWidth(SceneENUM.BUTTON_GRID.getVal());
-        this.setMaxWidth(SceneENUM.BUTTON_GRID.getVal());
+        this.setMinWidth(Integer.parseInt(myResources.getString("Button_Grid")));
+        this.setMaxWidth(Integer.parseInt(myResources.getString("Button_Grid")));
         this.setOnAction(event);
     }
 }
