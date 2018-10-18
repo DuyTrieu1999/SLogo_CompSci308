697260526d03a639a56942064f8b109a669f9969
Allen Qiu <asq3@duke.edu>
2018-10-17 22:41:24 -0400

    Command classes

---
+++ src/commands/If.java
@@ -0,0 +1,12 @@
+package commands;
+
+import java.util.Scanner;
+
+public class If implements Command {
+
+    @Override
+    public void run(Scanner args){
+        System.out.println("If");
+    }
+
+}
---
+++ src/commands/IfElse.java
@@ -0,0 +1,12 @@
+package commands;
+
+import java.util.Scanner;
+
+public class IfElse implements Command {
+
+    @Override
+    public void run(Scanner args){
+        System.out.println("If else");
+    }
+
+}
