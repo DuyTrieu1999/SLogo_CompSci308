5ae6eb01902974c6ca42a257ea2ea48f3b8a1a5b
Allen Qiu <asq3@duke.edu>
2018-10-17 23:28:35 -0400

    Testing command classes

--- src/model/Main.java
+++ src/model/Main.java
@@ -9,12 +9,18 @@ import view.SLogoView;
  * @author duytrieu
  */
 public class Main extends Application{
+    /*
     @Override
     public void start (Stage stage) {
         SLogoView ui = new SLogoView();
         stage.setTitle("SLogo");
         stage.setScene(ui.sceneInit());
         stage.show();
+    }*/
+
+    @Override
+    public void start(Stage stage){
+        CommandParser.parse("left 50");
     }
 
     public static void main (String[] args) {
