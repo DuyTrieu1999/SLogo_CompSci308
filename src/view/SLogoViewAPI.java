0ccc840d38849201b937d4bfa4b7d0a2178d45c8
Brooke Keene <bzk2@duke.edu>
2018-10-17 23:14:47 -0400

    starting to define API

--- src/view/SLogoViewAPI.java
+++ src/view/SLogoViewAPI.java
@@ -1,7 +1,14 @@
 package view;
 
+import java.util.ArrayList;
+
 public interface SLogoViewAPI {
     public void clearConsole();
     public void clearDisplay();
     public void showMessage(String text);
+    public void resetSettings();
+    public void resetTurtle();
+    public ArrayList<String> getSelectedVars(); //TODO: change return type - don't yet know how we are storing variables
+    public String getHistory(); //TODO: change return type - don't yet know how we are storing history
+    public void clearHistory();
 }
