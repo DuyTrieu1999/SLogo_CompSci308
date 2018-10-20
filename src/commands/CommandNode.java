3b17d4c2f98b3219c3db200d0074e2c236c725f2
Yunhao Qing <yq50@duke.edu>
2018-10-20 17:51:13 -0400

    Developed Parser

--- src/commands/CommandNode.java
+++ src/commands/CommandNode.java
@@ -1,15 +1,30 @@
 package commands;
 
 import java.util.*;
+import model.Turtle;
 
 public class CommandNode {
 
     private int numChildren;
     private int numParameters;
     private List<CommandNode> children;
+    private List<String> extraParameters;
+    private Turtle turtle;
 
-    public CommandNode(){
+    public CommandNode(Turtle t){
         numChildren = 0;
         children = new ArrayList<>();
+        extraParameters = new ArrayList<>();
+        turtle = t;
     }
+
+    public int getNumParameters(){
+        return numParameters;
+    }
+
+    public void setExtraParameters(List<String> list){
+        extraParameters = list;
+    }
+
+
 }
---
+++ src/model/Parser.java
@@ -0,0 +1,31 @@
+package model;
+
+import commands.CommandNode;
+import java.util.*;
+
+public class Parser {
+    private List<CommandNode> commandList;
+
+    public Parser(String str){
+        String[] lines=str.split("\\r?\\n");
+        List<String> list = new ArrayList<>();
+        for (String line : lines){
+            if (!line.trim().isEmpty() && !(line.charAt(0)=='#')){
+                String[] array = line.split("\\s+");
+                list.addAll(Arrays.asList(array));
+            }
+        }
+        int count = 0;
+        while (count<list.size()){
+            CommandNode node;
+            //Somehow initilise the node with the string name;
+            List<String> parameters = new ArrayList<String>();
+            for (int i = count + 1; i <= count + node.getNumParameters();i++){
+                parameters.add(list.get(i));
+            }
+            node.setExtraParameters(parameters);
+        }
+    }
+
+
+}
