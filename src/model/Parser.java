0db69d320ba5c92cd871e034475eab9099a4fd9f
Yunhao Qing <yq50@duke.edu>
2018-10-20 20:32:17 -0400

    Done more

--- src/model/Parser.java
+++ src/model/Parser.java
@@ -1,21 +1,36 @@
 package model;
 
 import commands.CommandNode;
+
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Parser {
     private List<CommandNode> commandList;
 
     public Parser(String str){
-        String[] lines=str.split("\\r?\\n");
-        List<String> list = new ArrayList<>();
-        for (String line : lines){
+        List<String> lines = Arrays.asList(str.split("\\r?\\n"));
+        List<List<String>> list = new ArrayList<>();
+
+        int count = 0;
+        while (count < lines.size()){
+            String line = lines.get(count);
+            List<String> temp = new ArrayList<>();
             if (!line.trim().isEmpty() && !(line.charAt(0)=='#')){
-                String[] array = line.split("\\s+");
-                list.addAll(Arrays.asList(array));
+                temp.addAll(Arrays.asList(line.split("\\s+")));
+                while (!isBalance(temp)){
+                    count++;
+                    String newline = lines.get(count);
+                    if (!newline.trim().isEmpty() && !(newline.charAt(0)=='#')){
+                        temp.addAll(Arrays.asList(newline.split("\\s+")));
+                    }
+                }
+                list.add(temp);
             }
         }
-        int count = 0;
+
+
+        count = 0;
         while (count<list.size()){
             CommandNode node;
             //Somehow initilise the node with the string name;
@@ -27,5 +42,15 @@ public class Parser {
         }
     }
 
-
+    public boolean isBalance(List<String> strings){
+        int count1 = 0;
+        int count2 = 0;
+        for (String str : strings){
+            if (str.equals("["))
+                count1++;
+            if (str.equals("]"))
+                count2++;
+        }
+        return count1==count2;
+    }
 }
