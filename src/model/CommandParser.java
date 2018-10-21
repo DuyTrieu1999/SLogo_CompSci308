18dc73382e5274805000291a2192e211c8d5bba1
Brooke Keene <bzk2@duke.edu>
2018-10-20 23:08:13 -0400

    commented things out for testing

--- src/model/CommandParser.java
+++ src/model/CommandParser.java
@@ -1,34 +1,34 @@
-package model;
-
-import commands.Command;
-import commands.CommandInitializer;
-
-import java.util.Map;
-import java.util.ResourceBundle;
-import java.util.Scanner;
-
-public class CommandParser {
-
-    private CommandList myCommandList;
-    private VariableMap myVarMap;
-
-    public CommandParser(Turtle t, CommandList cl, VariableMap vm){
-        myCommandList = cl;
-        myVarMap = vm;
-        myTurtle = t;
-    }
-
-    public void parseWholeString(String allCommands){
-        myCommandList.addCommand(allCommands);
-        String[] commands = allCommands.split("\\r?\\n");
-        for (String command : commands)
-            parse(command);
-    }
-
-    public static void parse(String oneCommand){
-        CommandInitializer myCommands = new CommandInitializer(ResourceBundle.getBundle("languages/English"));
-        Map<String, Command> commandMap = myCommands.createCommandMap();
-        Scanner commandScanner = new Scanner(oneCommand);
-        commandMap.get(commandScanner.next()).run(commandScanner);
-    }
-}
+//package model;
+//
+//import commands.Command;
+//import commands.CommandInitializer;
+//
+//import java.util.Map;
+//import java.util.ResourceBundle;
+//import java.util.Scanner;
+//
+//public class CommandParser {
+//
+//    private CommandList myCommandList;
+//    private VariableMap myVarMap;
+//
+//    public CommandParser(Turtle t, CommandList cl, VariableMap vm){
+//        myCommandList = cl;
+//        myVarMap = vm;
+//        myTurtle = t;
+//    }
+//
+//    public void parseWholeString(String allCommands){
+//        myCommandList.addCommand(allCommands);
+//        String[] commands = allCommands.split("\\r?\\n");
+//        for (String command : commands)
+//            parse(command);
+//    }
+//
+//    public static void parse(String oneCommand){
+//        CommandInitializer myCommands = new CommandInitializer(ResourceBundle.getBundle("languages/English"));
+//        Map<String, Command> commandMap = myCommands.createCommandMap();
+//        Scanner commandScanner = new Scanner(oneCommand);
+//        commandMap.get(commandScanner.next()).run(commandScanner);
+//    }
+//}
