e2f5af58fa0744a3449c37acb7409d93d6174ff7
Yunhao Qing <yunhao.qing@duke.edu>
2018-10-20 20:53:56 -0400

    Update CommandParser.java

--- src/model/CommandParser.java
+++ src/model/CommandParser.java
@@ -5,41 +5,31 @@ import commands.CommandNode;
 import java.lang.reflect.Array;
 import java.util.*;
 
-public class Parser {
+public class CommandParser {
     private List<CommandNode> commandList;
 
-    public Parser(String str){
+    public CommandParser(String str){
         List<String> lines = Arrays.asList(str.split("\\r?\\n"));
+        List<String> cleanLines = new ArrayList<>();
+        for (String line : lines){
+            if (!line.trim().isEmpty() && !(line.charAt(0)=='#'))
+                cleanLines.add(line);
+        }
         List<List<String>> list = new ArrayList<>();
-
         int count = 0;
         while (count < lines.size()){
             String line = lines.get(count);
             List<String> temp = new ArrayList<>();
-            if (!line.trim().isEmpty() && !(line.charAt(0)=='#')){
-                temp.addAll(Arrays.asList(line.split("\\s+")));
-                while (!isBalance(temp)){
-                    count++;
-                    String newline = lines.get(count);
-                    if (!newline.trim().isEmpty() && !(newline.charAt(0)=='#')){
-                        temp.addAll(Arrays.asList(newline.split("\\s+")));
-                    }
-                }
+            temp.addAll(Arrays.asList(line.split("\\s+")));
+            while (!isBalance(temp)){
+                count++;
+                String newline = lines.get(count);
+                temp.addAll(Arrays.asList(newline.split("\\s+")));
+                count++;
                 list.add(temp);
             }
         }
-
-
-        count = 0;
-        while (count<list.size()){
-            CommandNode node;
-            //Somehow initilise the node with the string name;
-            List<String> parameters = new ArrayList<String>();
-            for (int i = count + 1; i <= count + node.getNumParameters();i++){
-                parameters.add(list.get(i));
-            }
-            node.setExtraParameters(parameters);
-        }
+        
     }
 
     public boolean isBalance(List<String> strings){
