93087e88516796ae334df986be689dcf696ca368
Yunhao Qing <yunhao.qing@duke.edu>
2018-10-20 20:39:18 -0400

    Update CommandParser.java

--- src/model/CommandParser.java
+++ src/model/CommandParser.java
@@ -1,34 +1,56 @@
 package model;
 
-import commands.CommandInitializer;
 import commands.CommandNode;
 
-import java.util.Map;
-import java.util.ResourceBundle;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
-public class CommandParser {
+public class Parser {
+    private List<CommandNode> commandList;
 
-    private CommandList myCommandList;
-    private VariableMap myVarMap;
+    public Parser(String str){
+        List<String> lines = Arrays.asList(str.split("\\r?\\n"));
+        List<List<String>> list = new ArrayList<>();
 
-    public CommandParser(Turtle t, CommandList cl, VariableMap vm){
-        myCommandList = cl;
-        myVarMap = vm;
-        myTurtle = t;
+        int count = 0;
+        while (count < lines.size()){
+            String line = lines.get(count);
+            List<String> temp = new ArrayList<>();
+            if (!line.trim().isEmpty() && !(line.charAt(0)=='#')){
+                temp.addAll(Arrays.asList(line.split("\\s+")));
+                while (!isBalance(temp)){
+                    count++;
+                    String newline = lines.get(count);
+                    if (!newline.trim().isEmpty() && !(newline.charAt(0)=='#')){
+                        temp.addAll(Arrays.asList(newline.split("\\s+")));
+                    }
+                }
+                list.add(temp);
+            }
+        }
+
+
+        count = 0;
+        while (count<list.size()){
+            CommandNode node;
+            //Somehow initilise the node with the string name;
+            List<String> parameters = new ArrayList<String>();
+            for (int i = count + 1; i <= count + node.getNumParameters();i++){
+                parameters.add(list.get(i));
+            }
+            node.setExtraParameters(parameters);
+        }
     }
 
-    public void parseWholeString(String allCommands){
-        myCommandList.addCommand(allCommands);
-        String[] commands = allCommands.split("\\r?\\n");
-        for (String command : commands)
-            parse(command);
-    }
-
-    public void parse(String oneCommand){
-        CommandInitializer myCommands = new CommandInitializer(ResourceBundle.getBundle("languages/English"));
-        Map<String, CommandNode> commandMap = myCommands.createCommandMap(myTurtle);
-        Scanner commandScanner = new Scanner(oneCommand);
-        commandMap.get(commandScanner.next());
+    public boolean isBalance(List<String> strings){
+        int count1 = 0;
+        int count2 = 0;
+        for (String str : strings){
+            if (str.equals("["))
+                count1++;
+            if (str.equals("]"))
+                count2++;
+        }
+        return count1==count2;
     }
 }
