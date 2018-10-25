564c7f91d77c3385255422bdf3f11e84ea048ad4
Brooke Keene <bzk2@duke.edu>
2018-10-24 22:06:38 -0400

    implementing command history

--- src/model/CommandList.java
+++ src/model/CommandList.java
@@ -4,20 +4,32 @@ package model;
 import commands.CommandInitializer;
 import controller.Controller;
 
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * @Author Yunhao Qing
  * @Author Duy Trieu
+ * @Author Brooke Keene
  */
 
 public class CommandList implements CommandListInterface{
     private CommandParser myParser;
     private Controller myController;
+    private Queue<String> myHistory;
 
     public CommandList(Controller controller) {
         myController = controller;
         myParser = new CommandParser();
+        myHistory = new LinkedList<>();
+    }
+
+    public void addCommand (String newCommand) {
+        myHistory.add(newCommand);
+    }
+
+    public Queue<String> getHistory() {
+        return myHistory;
     }
 
     public void parse(String text) {
