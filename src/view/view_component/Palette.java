c55dc403727645eddfe4382bfdd16dfe5c51ab5f
Brooke Keene <bzk2@duke.edu>
2018-10-27 20:54:07 -0400

    implementing indexed palette

---
+++ src/view/view_component/Palette.java
@@ -0,0 +1,13 @@
+package view.view_component;
+
+import java.util.HashMap;
+
+/**
+ * Palette
+ *
+ * @author brookekeene
+ */
+public class Palette extends HashMap<Integer, String> {
+    public Palette() {}
+    
+}
